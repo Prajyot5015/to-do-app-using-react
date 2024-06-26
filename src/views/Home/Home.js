dc84e70aafb67e1dab1b10d6d7056e50410184bc
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-06-26 21:14:14 +0530

    Added Home

---
+++ src/views/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+        <h1 className='app-title'>TO-DO App</h1>
+
+        <div className='todo-list-container'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
