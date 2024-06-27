d3118d53dfd8d184176c90efcaf45ef17b0d2a20
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-06-27 17:36:19 +0530

    Modified Home.js

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -5,16 +5,16 @@ import AddPng from './add.png'
 function Home() {
   return (
     <div>
-        <h1 className='app-title'>TO-DO App</h1>
+      <h1 className='app-title'>TO-DO App</h1>
 
-        <div className='todo-list-container'>
-            <h2>List Will go here</h2>
-        </div>
+      <div className='todo-list-container'>
+        <h2>List Will go here</h2>
+      </div>
 
-        <div>
-            <input type='text' className='add-input' placeholder='Add new Task'/>
-            <img src={AddPng} alt='add' className='add-icon' />
-        </div>
+      <div className='add-todo-item-container'>
+        <input type='text' className='add-input' placeholder='Add new Task' />
+        <img src={AddPng} alt='add' className='add-icon' />
+      </div>
     </div>
   )
 }
